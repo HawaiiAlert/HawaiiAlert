65e41f6e05b4407338f9c37d2fb157490ec3e301
lyuyeda <luyeda6@hawaii.edu>
2018-03-16 01:35:52 -1000

    Style css (#1)
    * Meteor built
    
    * Dynamic Templates
    
    This has only been partially implemented, it does not contain full list of options.  More templates are needed, styling is incomplete.
    
    * add style to drill disaster alerts
    
    * Adding alert and summary
    
    This is a conversion of Kalen's work being merged with Yizan's.  It is not yet complete.
    
    * Cleanup
    
    A few inconsequential changes to improve readability.  Separated to avoid confusion in later commits.
    
    * Filling in some blanks
    
    Added in a confirmation after the summary that I had in my emailed file. Filled out the buttons that all said "missile." Still working on it right now.
    
    * Interface v1
    
    Initial, hard-coded version of the interface.  Will change to be dynamic later.
    
    * User sees if they canceled
    
    When they go back to first screen they'll see whether or not they canceled.
    
    * README

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 An app to improve on Hawaii's disaster alert systems
 
 To install
-1. Clone the repository to your computer
-1. Open a terminal and `cd` to the 'app' folder
-1. Run `meteor npm install`
-1. Run `meteor npm run start`
-1. The app will be running at localhost:3000
+1. [Install Meteor](https://www.meteor.com/install).
+1. Download, or fork and clone, the repository to your computer.
+1. Open a terminal and `cd` to the `app/` directory.
+1. Run `meteor npm install`.
+1. Run `meteor run`.
+1. The app will be running at [localhost:3000](http://localhost:3000).
+
+
+The options below have not yet been implemented
+
+If you would like to change the set of options, the initial database can be found at `app/private/database/initial-collection-data.json`.
+After making any changes to this file, please run `meteor reset` in the `app/` directory to clear out the previous database.
+
+As `app/settings.json` and `app/private/database/initial-collection-data.json` contain sensitive information, it is recommended that you restrict read/write access to these files.
---
+++ app/client/main.js
@@ -0,0 +1,224 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+import './main.html';
+
+/*The following will store options, the page will then generate based on them, allowing new options to be added quickly*/
+/*
+var options = {
+  "drill" = {},
+  "disaster" = {},
+  "locations" = {},
+  "alerts" = {},
+};
+*/
+var stage = null;
+var cancelled = false;
+/*The following stores the user's selections from the interface*/
+var selection = {
+  "drill": null,
+  "disaster": null,
+  "locations": [],
+  "alerts": [],
+  };
+
+
+/////Interface/////
+Template.interface.onCreated(function onCreated(){
+  stage = "drill";
+  cancelled = false;
+  selection = {
+    "drill": null,
+    "disaster": null,
+    "locations": [],
+    "alerts": []
+    };
+  BlazeLayout.render('load', {"stage":stage});
+});
+
+
+/////Drill/////
+Template.drill.events({
+  'click #drill'(event, instance) {
+    stage="disaster";
+    selection.drill="A Drill";
+    cancelled = false;
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #not_drill'(event, instance) {
+    stage="disaster";
+    selection.drill="Not a Drill";
+    cancelled = false;
+    BlazeLayout.render('load', {"stage":stage});
+  },
+});
+
+Template.drill.helpers({
+  cancelled_last() {
+    if (cancelled){
+      return "Prior Alert canceled";
+    }
+  },
+});
+
+
+/////Disaster/////
+Template.disaster.events({
+  'click #missile'(event, instance) {
+    stage="location";
+    selection.disaster="Missile";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #hurricane'(event, instance) {
+    stage="location";
+    selection.disaster="Hurricane";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #flooding'(event, instance) {
+    stage="location";
+    selection.disaster="Flooding";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #tsunami'(event, instance) {
+    stage="location";
+    selection.disaster="Tsunami";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #wildfire'(event, instance) {
+    stage="location";
+    selection.disaster="Wildfire";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #earthquake'(event, instance) {
+    stage="location";
+    selection.disaster="Earthquake";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #terrorist'(event, instance) {
+    stage="location";
+    selection.disaster="Terrorist Attack";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #meteor'(event, instance) {
+    stage="location";
+    selection.disaster="Meteor";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #back'(event, instance) {
+    stage="drill";
+    selection.disaster=null;
+    BlazeLayout.render('load', {"stage":stage});
+  },
+});
+
+
+/////Location/////
+Template.location.events({
+  'click #submit'(event, instance) {
+    stage="alerts";
+    if (oahu.checked) {
+      selection.locations.push("Oahu");
+    }
+    if (maui.checked) {
+      selection.locations.push("Maui");
+    }
+    if (hawaii.checked) {
+      selection.locations.push("Hawaii Island");
+    }
+    if (kauai.checked) {
+      selection.locations.push("Kauai");
+    }
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #back'(event, instance) {
+    stage="disaster";
+    selection.locations=[];
+    BlazeLayout.render('load', {"stage":stage});
+  },
+});
+
+
+/////Alerts/////
+Template.alerts.events({
+  'click #submit'(event, instance) {
+    stage="summary";
+    if (text.checked) {
+      selection.alerts.push("Text Alert");
+    }
+    if (tv.checked) {
+      selection.alerts.push("TV Alert");
+    }
+    if (radio.checked) {
+      selection.alerts.push("Radio Alert");
+    }
+    if (siren.checked) {
+      selection.alerts.push("Warning Sirens");
+    }
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #back'(event, instance) {
+    stage="location";
+    selection.alerts=[];
+    BlazeLayout.render('load', {"stage":stage});
+  },
+});
+
+
+/////Summary/////
+Template.summary.helpers({
+  drill(){
+    return selection.drill;
+  },
+  disaster(){
+    return selection.disaster;
+  },
+  locations(){
+    return selection.locations;
+  },
+  alerts(){
+    return selection.alerts;
+  }
+});
+
+Template.summary.events({
+  'click #confirm'(event, instance) {
+    stage="confirmation";
+    BlazeLayout.render('load', {"stage":stage});
+  },
+  'click #cancel'(event, instance) {
+    stage="drill";
+    selection.drill=null;
+    selection.disaster=null;
+    selection.locations=[];
+    selection.alerts=[];
+    canceled=true;
+    BlazeLayout.render('load', {"stage":stage});
+  },
+});
+
+
+/////Confirmation/////
+Template.confirmation.helpers({
+  drill(){
+    return selection.drill;
+  }
+});
+
+Template.confirmation.events({
+  'submit form': function(event){
+    event.preventDefault();
+    if(event.target.password.value=="password"&&event.target.drill.value==selection.drill){
+      console.log(selection);
+    }
+  },
+  'click #cancel'(event, instance) {
+    stage="drill";
+    selection.drill=null;
+    selection.disaster=null;
+    selection.locations=[];
+    selection.alerts=[];
+    canceled=true;
+    BlazeLayout.render('load', {"stage":stage});
+  },
+});
